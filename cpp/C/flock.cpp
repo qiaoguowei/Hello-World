c60b76d7c78cdb52bdcade1ca3efb7cf17a07e7b
qiaoguowei <qiaoguoweixxx@qq.com>
2021-12-26 15:22:35 +0800

    11111

---
+++ cpp/C/flock.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/file.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string>
+
+const std::string FileName = "./test.log";
+
+int main()
+{
+	int nIndex = 0;
+	int fd = open(FileName.c_str(), O_CREAT | O_APPEND | O_RDWR, 0664);
+	if(fd <= 0)
+	{
+		printf("open file fail\n");
+		return -1;
+	}
+	while(nIndex++ < 30)
+	{
+		if(0 == flock(fd, LOCK_EX | LOCK_NB))
+		{
+			dprintf(fd, "do something %d, %d\n", getpid(), nIndex);
+			sleep(1);
+			flock(fd, LOCK_UN);
+		}
+	}
+#if 0
+	if(0 == flock(fd, LOCK_EX))//| LOCK_NB))
+	{
+		while(nIndex++ < 30)
+		{
+			dprintf(fd, "do something %d, %d\n", getpid(), nIndex);
+		}
+	}
+	flock(fd, LOCK_UN);
+#endif
+	close(fd);
+	return 0;
+}
